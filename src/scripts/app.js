1b1725efdbe72bf01a6f4650b1ece479db370346
Dmytro Poltavchenko <dmytro.poltavchenko@bystrov.agency>
2020-02-19 15:32:20 +0200

    init upload

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,13 @@
 import $ from 'jquery';
 import { load } from '@shopify/theme-sections';
+import 'Scripts/jquery.plugins.js'
 
 class App {
   constructor() {
-
+    this.init()
+  }
+  init() {
+    
   }
 }
 
