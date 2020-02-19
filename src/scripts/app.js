88e59256e8aefa87dcd8a8146d64044b8106b9f0
Dmytro Poltavchenko <dmytro.poltavchenko@bystrov.agency>
2020-02-19 18:01:42 +0200

    upload

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,13 @@
 import $ from 'jquery';
 import { load } from '@shopify/theme-sections';
+
+import 'lazysizes/plugins/object-fit/ls.object-fit';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+import 'lazysizes/plugins/rias/ls.rias';
+import 'lazysizes/plugins/bgset/ls.bgset';
+import 'lazysizes';
+import 'lazysizes/plugins/respimg/ls.respimg';
+
 import 'Scripts/jquery.plugins.js'
 
 class App {
@@ -7,7 +15,16 @@ class App {
     this.init()
   }
   init() {
-    
+    // initGallery();
+
+    // Responsive fluid iframe
+    $(".rte iframe").each(function (index) {
+      $(this).wrap('<div class="fluid-iframe"></div>');
+    });
+
+    if (!('ontouchstart' in document.documentElement)) {
+      $('html').addClass('no-touch');
+    }
   }
 }
 
