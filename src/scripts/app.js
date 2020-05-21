d38f3c256029039ba463ba3b3c6146b831cded0f
Dmytro Poltavchenko <dmytro.poltavchenko@bystrov.agency>
2020-05-21 11:45:38 +0300

    upload

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -21,6 +21,8 @@ class App {
     this.init()
   }
   init() {
+    this.initIosScroll();
+    this.initMobileNav();
 
     // Responsive fluid iframe
     $(".rte iframe").each(function (index) {
@@ -30,6 +32,65 @@ class App {
     if (!('ontouchstart' in document.documentElement)) {
       $('html').addClass('no-touch');
     }
+  }
+  
+  initIosScroll() {
+    ResponsiveHelper.addRange({
+      '..1199': {
+        on: function() {
+          let $docEl = $('html, body'),
+            $wrap = $('.page-wrapper'),
+            scrollTop;
+          $(".page-wrapper__opener").on("click", function(e) {
+            if ($('body').hasClass("scroll-fix")) {
+              $.unlockBody();
+              $('body').removeClass("scroll-fix");
+            } else {
+              $.lockBody();
+              $('body').addClass("scroll-fix");
+            }
+          })
+          $.unlockBody = function() {
+            $docEl.css({
+              height: "",
+              overflow: ""
+            });
+            $wrap.css({
+              top: ''
+            });
+            window.scrollTo(0, scrollTop);
+            window.setTimeout(function() {
+              scrollTop = null;
+            }, 0);
+          }
+          $.lockBody = function() {
+            if (window.pageYOffset) {
+              scrollTop = window.pageYOffset;
+              $wrap.css({
+                top: -(scrollTop)
+              });
+            }
+            $docEl.css({
+              height: "100%",
+              overflow: "hidden"
+            });
+          }
+        },
+        off: function() {
+          $(".page-wrapper__opener").off();
+        }
+      }
+    });
+  }
+  
+  // mobile menu init
+  initMobileNav() {
+    $('body').mobileNav({
+      menuActiveClass: 'nav-active',
+      menuOpener: '.menu__opener',
+      menuDrop: '.menu',
+      hideOnClickOutside: true
+    });
   }
 }
 
