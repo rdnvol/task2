a3f727bf949ac03355497c7ebec3eafea0365513
Dmytro Poltavchenko <dmytro.poltavchenko@bystrov.agency>
2020-05-25 19:58:37 +0300

    upload

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -21,6 +21,8 @@ class App {
     this.init()
   }
   init() {
+    this.initStickyScrollBlock();
+    this.initHeaderOnScrollDown();
     this.initIosScroll();
     this.initMobileNav();
 
@@ -31,6 +33,57 @@ class App {
 
     if (!('ontouchstart' in document.documentElement)) {
       $('html').addClass('no-touch');
+    }
+  }
+  
+  // initialize fixed blocks on scroll
+  initStickyScrollBlock() {
+    $('.header__panel').stickyScrollBlock({
+      setBoxHeight: true,
+      activeClass: 'fixed-position',
+      container: '.page-wrapper',
+      positionType: 'fixed',
+      animDelay: 0,
+      showAfterScrolled: true
+    });
+  }
+
+  // Hide Header on on scroll down
+  initHeaderOnScrollDown() {
+    let didScroll;
+    let lastScrollTop = 0;
+    let delta = 5;
+    let navbarHeight = $('.sticky-wrap-header__panel').outerHeight();
+
+    $(window).scroll(function(event){
+      didScroll = true;
+    });
+
+    setInterval(function () {
+      if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+      }
+    }, 250);
+
+    function hasScrolled() {
+      let st = $(window).scrollTop();
+      // Make sure they scroll more than delta
+
+      if (Math.abs(lastScrollTop - st) <= delta)
+        return;
+      // If they scrolled down and are past the navbar, add class .nav-up.
+      // This is necessary so you never see what is "behind" the navbar.
+      if (st > lastScrollTop && st > navbarHeight) {
+        // Scroll Down
+        $('.sticky-wrap-header__panel').removeClass('nav-down').addClass('nav-up');
+      } else {
+        // Scroll Up
+        if (st + $(window).height() < $(document).height()) {
+          $('.sticky-wrap-header__panel').removeClass('nav-up').addClass('nav-down');
+        }
+      }
+      lastScrollTop = st;
     }
   }
   
@@ -72,7 +125,8 @@ class App {
             }
             $docEl.css({
               height: "100%",
-              overflow: "hidden"
+              overflow: "hidden",
+              padding: "0"
             });
           }
         },
@@ -86,10 +140,10 @@ class App {
   // mobile menu init
   initMobileNav() {
     $('body').mobileNav({
-      menuActiveClass: 'nav-active',
+      menuActiveClass: 'menu-active',
       menuOpener: '.menu__opener',
       menuDrop: '.menu',
-      hideOnClickOutside: true
+      hideOnClickOutside: false
     });
   }
 }
