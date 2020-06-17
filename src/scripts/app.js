d86db387b65bd2e09cc5f99282a39dec61f188be
Anton Volchenkov <anton.volchenkov@bystrov.agency>
2020-06-17 18:51:59 +0300

    'Video' section markup close #133

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -13,6 +13,7 @@ import Swiper from 'swiper';
 import fancybox from '@fancyapps/fancybox';
 import product from "Scripts/product";
 import "Scripts/related-product";
+import bgVideo from 'jquery-background-video';
 
 import { load } from '@shopify/theme-sections';
 load('*');
@@ -45,6 +46,7 @@ class App {
     this.initProductGallery();
     this.initFancyboxAutoLoad();
     this.initTestimonialsSlider();
+    this.initBackgroundVideo();
 
     // Responsive fluid iframe
     $(".rte iframe").each(function(index) {
@@ -291,6 +293,15 @@ class App {
           spaceBetween: 60,
         },
       }
+    });
+  }
+
+  initBackgroundVideo() {
+    $('.jquery-background-video').bgVideo({
+      fadeIn: 1000,
+      showPausePlay: true,
+      pausePlayXPos: 'right',
+      pausePlayYPos: 'top'
     });
   }
 }
