f907c3d56309f5bd274c69e84a444459f06fa0a3
Andrii Hrohodza <50321565+AndriiHrohodza@users.noreply.github.com>
2021-06-30 11:23:52 +0300

    Deleted d-none from div with item options and properties in LimeItem.jsx close #618

--- src/scripts/Components/LineItem.jsx
+++ src/scripts/Components/LineItem.jsx
@@ -6,19 +6,19 @@ class LineItem extends Component {
   constructor(props) {
     super(props);
   }
-  
+
   removeItem = e => {
     e.preventDefault();
     let item = this.props.item;
     this.props.removeItem(item.key);
   }
-  
+
   updateItem = e => {
     let key = this.props.item.key;
     let quantity = +e.target.value;
     this.props.updateItem(key, { quantity });
   }
-  
+
   changeQuantity(action, element) {
     let changeEvent = new Event('change');
     switch (action) {
@@ -31,7 +31,7 @@ class LineItem extends Component {
         element.dispatchEvent(changeEvent);
     }
   }
-  
+
   renderImage({ image, title, url }) {
     return (
       <div className="cart__product-img">
@@ -50,7 +50,7 @@ class LineItem extends Component {
       </div>
     )
   }
-  
+
   renderProperties(properties) {
     for (var key in properties) {
       if (properties.hasOwnProperty(key)) {
@@ -63,7 +63,7 @@ class LineItem extends Component {
       }
     }
   }
-  
+
   renderPrice({ original_line_price, line_price, original_price, price }) {
     if (original_line_price !== line_price) {
       return (
@@ -84,7 +84,7 @@ class LineItem extends Component {
       )
     }
   }
-  
+
   renderItemOptions({ options_with_values, product_has_only_default_variant }) {
     if (!product_has_only_default_variant) {
       return options_with_values.map(option =>
@@ -92,7 +92,7 @@ class LineItem extends Component {
       )
     }
   }
-  
+
   render({ item, updateCart }) {
     const {
       key,
@@ -136,7 +136,7 @@ class LineItem extends Component {
                         onClick={ () => this.changeQuantity('-', this.mobileQuantityInput) }/>
                 </span>
               </div>
-              <div className="d-none">
+              <div>
                 { this.renderItemOptions(item) }
                 { this.renderProperties(properties) }
               </div>
