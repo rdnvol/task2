8df6bccbdced99e070452c5a4d35892966db96da
Dmytro Poltavchenko <dmytro.poltavchenko@bystrov.agency>
2021-09-30 10:11:23 +0300

    Replace Bootstrap grid by Tailwind grid #683 - add test markup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
+  mode: 'jit',
   purge: {
     enabled: false,
     content: [
-      './layout/*.liquid',
-      './templates/*.liquid',
-      './sections/*.liquid',
-      './snippets/*.liquid',
+      './**/*.liquid',
+      './**/Components/*.js',
+      './**/Components/*.jsx',
     ],
   },
   darkMode: false, // or 'media' or 'class'
@@ -17,7 +17,14 @@ module.exports = {
       'lg': '1024px',
       'xl': '1200px',
       '2xl': '1400px',
-    }
+    },
+    colors: {
+      // Build your palette here
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+    },
   },
   variants: {
     extend: {},
