a7f74753e445cb0b8f2329426f72cc42b70ce12b
Zaven <zaven.avanesyan93@gmail.com>
2021-10-05 12:06:10 +0400

    Prettier correction

--- src/scripts/templates/product.variants-swatcher.ts
+++ src/scripts/templates/product.variants-swatcher.ts
@@ -1,4 +1,4 @@
-import { load } from "@shopify/theme-sections";
-import "Sections/product-variants-swatcher";
+import { load } from '@shopify/theme-sections';
+import 'Sections/product-variants-swatcher';
 
-load("*");
+load('*');
