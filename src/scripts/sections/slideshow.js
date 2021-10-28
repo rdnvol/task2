dada364d4e80b30ab5a87556f063a6e3831b3a78
Dmytro Poltavchenko <dmytro.poltavchenko@bystrov.agency>
2021-10-28 16:06:16 +0300

    Update gallery from swiper to splide #760 - update home slideshow

--- src/scripts/sections/slideshow.js
+++ src/scripts/sections/slideshow.js
@@ -1,5 +1,5 @@
 import { register } from '@shopify/theme-sections';
-import Swiper from 'swiper';
+import Splide from '@splidejs/splide';
 
 register('slideshow', {
   _slideToBlock: function (index) {
@@ -7,33 +7,58 @@ register('slideshow', {
   },
 
   initTestimonialsSlider: function () {
+    // let delay = this.container.dataset.slidesEvery * 1000;
+    // let autorotate = this.container.dataset.autorotate === 'true';
+    // let blocksLength = +this.container.dataset.blocks;
+    // let loop = blocksLength > 1;
+    // console.log('loop', loop);
+    // let autoplay = autorotate ? { delay } : false;
+    // this.swiper = new Swiper(
+    //   this.container.querySelector('.swiper-container1'),
+    //   {
+    //     speed: 800,
+    //     watchOverflow: true,
+    //     loop,
+    //     autoplay,
+    //     effect: 'fade',
+    //     fadeEffect: {
+    //       crossFade: true,
+    //     },
+    //     pagination: {
+    //       el: '.swiper-pagination',
+    //       clickable: true,
+    //     },
+    //     navigation: {
+    //       nextEl: '.swiper-button-next',
+    //       prevEl: '.swiper-button-prev',
+    //     },
+    //   }
+    // );
+    
     let delay = this.container.dataset.slidesEvery * 1000;
     let autorotate = this.container.dataset.autorotate === 'true';
     let blocksLength = +this.container.dataset.blocks;
-    let loop = blocksLength > 1;
-    console.log('loop', loop);
+    let rewind = blocksLength > 1;
     let autoplay = autorotate ? { delay } : false;
-    this.swiper = new Swiper(
-      this.container.querySelector('.swiper-container'),
-      {
-        speed: 800,
-        watchOverflow: true,
-        loop,
-        autoplay,
-        effect: 'fade',
-        fadeEffect: {
-          crossFade: true,
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      }
-    );
+    console.log('autoplay', autoplay);
+    console.log('rewind', rewind);
+    console.log('delay', delay);
+
+    const testimonialsSlider = document.querySelector('.slideshow-gallery');
+    if (!testimonialsSlider) return;
+    this.swiper = new Splide(testimonialsSlider, {
+      type: 'fade',
+      speed: 800,
+      rewind,
+      autoplay,
+      // autoplay: true,
+      interval: delay,
+      perPage: 1,
+      gap: 0,
+      pagination: true,
+      arrows: true,
+      keyboard: 'focused',
+    }).mount();
   },
 
   // Shortcut function called when a section is loaded via 'sections.load()' or by the Theme Editor 'shopify:section:load' event.
