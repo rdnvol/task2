24f9c2b1bff3e510a0666492715d46ba8f3ec685
Dmytro Poltavchenko <dmytro.poltavchenko@bystrov.agency>
2021-10-29 11:34:21 +0300

    Update gallery from swiper to splide close #760

--- src/scripts/sections/slideshow.js
+++ src/scripts/sections/slideshow.js
@@ -3,55 +3,22 @@ import Splide from '@splidejs/splide';
 
 register('slideshow', {
   _slideToBlock: function (index) {
-    this.swiper.slideTo(index);
+    this.splide.go(index);
   },
 
-  initTestimonialsSlider: function () {
-    // let delay = this.container.dataset.slidesEvery * 1000;
-    // let autorotate = this.container.dataset.autorotate === 'true';
-    // let blocksLength = +this.container.dataset.blocks;
-    // let loop = blocksLength > 1;
-    // console.log('loop', loop);
-    // let autoplay = autorotate ? { delay } : false;
-    // this.swiper = new Swiper(
-    //   this.container.querySelector('.swiper-container1'),
-    //   {
-    //     speed: 800,
-    //     watchOverflow: true,
-    //     loop,
-    //     autoplay,
-    //     effect: 'fade',
-    //     fadeEffect: {
-    //       crossFade: true,
-    //     },
-    //     pagination: {
-    //       el: '.swiper-pagination',
-    //       clickable: true,
-    //     },
-    //     navigation: {
-    //       nextEl: '.swiper-button-next',
-    //       prevEl: '.swiper-button-prev',
-    //     },
-    //   }
-    // );
-    
+  initSlider: function () {
     let delay = this.container.dataset.slidesEvery * 1000;
     let autorotate = this.container.dataset.autorotate === 'true';
     let blocksLength = +this.container.dataset.blocks;
     let rewind = blocksLength > 1;
     let autoplay = autorotate ? { delay } : false;
-    console.log('autoplay', autoplay);
-    console.log('rewind', rewind);
-    console.log('delay', delay);
 
-    const testimonialsSlider = document.querySelector('.slideshow-gallery');
-    if (!testimonialsSlider) return;
-    this.swiper = new Splide(testimonialsSlider, {
+    this.splide = new Splide(
+      this.container.querySelector('.slideshow-gallery'), {
       type: 'fade',
       speed: 800,
       rewind,
       autoplay,
-      // autoplay: true,
       interval: delay,
       perPage: 1,
       gap: 0,
@@ -63,9 +30,7 @@ register('slideshow', {
 
   // Shortcut function called when a section is loaded via 'sections.load()' or by the Theme Editor 'shopify:section:load' event.
   onLoad: function (e) {
-    console.log('slideshow init');
-    console.log(this);
-    this.initTestimonialsSlider();
+    this.initSlider();
   },
 
   // Shortcut function called when a section is selected by the Theme Editor 'shopify:section:select' event.
@@ -75,13 +40,13 @@ register('slideshow', {
 
   // Shortcut function called when a section unloaded by the Theme Editor 'shopify:section:unload' event.
   onUnload: function (e) {
-    this.swiper.destroy();
+    this.splide.destroy();
     // Do something when a section instance is unloaded
   },
 
   // Shortcut function called when a section is deselected by the Theme Editor 'shopify:section:deselect' event.
   onDeselect: function () {
-    this.swiper.slideTo(1);
+    this.splide.go(1);
     // Do something when a section instance is deselected
   },
   // Shortcut function called when a section block is selected by the Theme Editor 'shopify:block:select' event.
@@ -92,7 +57,7 @@ register('slideshow', {
 
   // Shortcut function called when a section block is deselected by the Theme Editor 'shopify:block:deselect' event.
   onBlockDeselect: function (e) {
-    this.swiper.slideTo(1);
+    this.splide.go(1);
     // Do something when a section block is deselected
   },
 });
