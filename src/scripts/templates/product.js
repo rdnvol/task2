2af1bbca292b6e059b6676471c4b4b06a547e1d1
Andrii Hrohodza <50321565+AndriiHrohodza@users.noreply.github.com>
2021-11-09 17:39:13 +0200

    Added testimonials to product js close #925

--- src/scripts/templates/product.js
+++ src/scripts/templates/product.js
@@ -2,5 +2,6 @@ import { load } from '@shopify/theme-sections';
 import 'Sections/product';
 import 'Sections/related-product';
 import 'Sections/iframe-video';
+import 'Sections/testimonials';
 
 load('*');
