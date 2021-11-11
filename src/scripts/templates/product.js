eb77c3d7cd9de1bd9c7e0a5ec3175243c68991fe
Zaven <zaven.avanesyan93@gmail.com>
2021-11-11 15:55:53 +0400

    Section video - PDP

--- src/scripts/templates/product.js
+++ src/scripts/templates/product.js
@@ -2,6 +2,7 @@ import { load } from '@shopify/theme-sections';
 import 'Sections/product';
 import 'Sections/related-product';
 import 'Sections/iframe-video';
+import 'Sections/video';
 import 'Sections/testimonials';
 import 'Sections/slideshow';
 
