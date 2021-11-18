969b40d3acc87dc6bd790763992988636a291371
Andrii Hrohodza <50321565+AndriiHrohodza@users.noreply.github.com>
2021-11-18 12:51:58 +0200

    Some fixes in product js close #1120

--- src/scripts/sections/product.js
+++ src/scripts/sections/product.js
@@ -73,6 +73,11 @@ export class Product {
 
   initVariantSelects() {
     this.variantSelects = document.getElementById('variant-selects');
+    this.productForm = document.querySelector(
+      `#product-form-${this.variantSelects.dataset.section}`
+    );
+    this.inputName = this.productForm.querySelector('input[name="id"]');
+    this.inputName.disabled = false;
     this.variantSelects.addEventListener(
       'change',
       this.onVariantChange.bind(this, this.variantSelects)
@@ -103,15 +108,8 @@ export class Product {
   }
 
   updateVariantInput(variant) {
-    const productForm = document.querySelector(
-      `#product-form-${this.variantSelects.dataset.section}`
-    );
-
-    const input = productForm.querySelector('input[name="id"]');
-
-    input.disabled = false;
-    input.value = variant.id;
-    input.dispatchEvent(new Event('change', { bubbles: true }));
+    this.inputName.value = variant.id;
+    this.inputName.dispatchEvent(new Event('change', { bubbles: true }));
   }
 
   initGallery() {
@@ -288,11 +286,10 @@ export class Product {
   };
 
   getVariantData() {
-    const variantSelects = document.getElementById('variant-selects');
     this.variantData =
       this.variantData ||
       JSON.parse(
-        variantSelects.querySelector('[type="application/json"]').textContent
+        this.variantSelects.querySelector('[type="application/json"]').textContent
       );
   }
 
