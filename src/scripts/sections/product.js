bbc00739426390c9f4f470a476f14f3eb745a57b
Andrii Hrohodza <50321565+AndriiHrohodza@users.noreply.github.com>
2022-04-15 10:40:27 +0300

    Rewrite updateVariantPrice

--- src/scripts/sections/product.js
+++ src/scripts/sections/product.js
@@ -191,14 +191,15 @@ export class Product {
 
   updateVariantPrice(variant) {
     this.priceContainer.empty();
+    while (this.priceContainer.firstChild) this.priceContainer.removeChild(this.priceContainer.firstChild);
 
     if (variant) {
       if (variant.compare_at_price > variant.price) {
-        this.priceContainer.append(`<ins>${formatMoney(variant.price, theme.moneyFormat)}</ins>`);
+        this.priceContainer.appendChild(`<ins>${formatMoney(variant.price, theme.moneyFormat)}</ins>`);
 
-        this.priceContainer.append(`<del>${formatMoney(variant.compare_at_price, theme.moneyFormat)}</del>`);
+        this.priceContainer.appendChild(`<del>${formatMoney(variant.compare_at_price, theme.moneyFormat)}</del>`);
       } else {
-        this.priceContainer.append(`<div>${formatMoney(variant.price, theme.moneyFormat)}</div>`);
+        this.priceContainer.appendChild(`<div>${formatMoney(variant.price, theme.moneyFormat)}</div>`);
       }
     }
   }
