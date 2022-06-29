f9b407c0f2a0a0020ebefb181923f10c2054a96e
lysenko-alex <alex91113011@gmail.com>
2022-06-29 20:08:39 +0300

    test

--- deploy.sh
+++ deploy.sh
@@ -46,14 +46,13 @@ YAML
 export CI=1
 export SHOPIFY_SHOP="${SHOP_STORE#*(https://|http://)}"
 
+
 echo $SHOP_ACCESS_TOKEN
 echo $SHOP_STORE
 echo $THEME_ROOT
 
 if [[ -n "$SHOP_ACCESS_TOKEN" ]]; then
   export SHOPIFY_PASSWORD="$SHOP_ACCESS_TOKEN"
-else
-  export SHOPIFY_PASSWORD="$SHOP_APP_PASSWORD"
 fi
 
 shopify login
@@ -66,8 +65,6 @@ export SHOPIFY_SHOP="${SHOP_STORE#*(https://|http://)}"
 
 if [[ -n "$SHOP_ACCESS_TOKEN" ]]; then
   export SHOPIFY_PASSWORD="$SHOP_ACCESS_TOKEN"
-else
-  export SHOPIFY_PASSWORD="$SHOP_APP_PASSWORD"
 fi
 
 shopify login
