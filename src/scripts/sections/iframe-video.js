0ec04bd30112e86958bf395d88eef55d95e9fa89
Andrii Hrohodza <50321565+AndriiHrohodza@users.noreply.github.com>
2022-07-27 16:14:38 +0300

    Updated iframe-video

--- src/scripts/sections/iframe-video.js
+++ src/scripts/sections/iframe-video.js
@@ -57,14 +57,6 @@ register('iframe-video', {
 
   // Shortcut function called when a section is loaded via 'sections.load()' or by the Theme Editor 'shopify:section:load' event.
   onLoad() {
-    const sectionName = `${this.container.getAttribute('data-section-type')}-${this.id}`;
-
-    performanceMeasure(sectionName, () => {
-      performance.mark(`${sectionName}-Start`);
-
-      this.initIframeVideo();
-
-      performance.mark(`${sectionName}-End`);
-    });
+    performanceMeasure(this.id, this.initIframeVideo.bind(this));
   },
 });
