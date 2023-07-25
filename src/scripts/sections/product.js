587f16aafe8646bd9b54d1b2fc4944f982be12a3
rdnvol <66828584+rdnvol@users.noreply.github.com>
2023-07-25 20:58:22 +0300

    the task issue/CU-85ztjbtyx is done

--- src/scripts/sections/product.js
+++ src/scripts/sections/product.js
@@ -7,8 +7,35 @@ register('product', {
     if (handle) window.Product = new Product(this.container);
   },
 
+  _initCcomplementaryProducts() {
+    const productRecommendationsSection = document.querySelector('.product-complementary');
+    const { url } = productRecommendationsSection.dataset;
+    const button = document.querySelector('#accordion5id').parentElement;
+
+    button.addEventListener('click', (event) => {
+      if (button.classList.contains('open')) {
+        fetch(url)
+          .then((response) => response.text())
+          .then((text) => {
+            const html = document.createElement('div');
+
+            html.innerHTML = text;
+            const recommendations = html.querySelector('.product-complementary');
+
+            if (recommendations && recommendations.innerHTML.trim().length) {
+              productRecommendationsSection.innerHTML = recommendations.innerHTML;
+            }
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+      }
+    });
+  },
+
   onLoad() {
     performanceMeasure(this.id, this._initProduct.bind(this, this.container.dataset.handle));
+    this._initCcomplementaryProducts();
   },
 
   onBlockSelect() {
